0ee59e3104b3cb2dd348cf045b8ffd1e5c6c4a53
baskiton <baskiton@mail.ru>
2021-09-26 02:10:39 +0700

    v0.1.1 minor

--- src/ext.c
+++ src/ext.c
@@ -7,6 +7,8 @@
 PyDoc_STRVAR(ext__doc__,
 "Wrapper for setns syscall.\n");
 
+#define AddZeroMacro(m, c) PyModule_AddIntConstant(m, #c, 0)
+
 PyDoc_STRVAR(setns__doc__,
 "setns(fd: int, nstype: int) -> None\n"
 "\n"
@@ -59,14 +61,54 @@ PyInit_ext(void)
         return NULL;
     }
 
+#ifdef CLONE_NEWTIME
     PyModule_AddIntMacro(module, CLONE_NEWTIME);
+#else
+# warning "CLONE_NEWTIME macro not found. Set to 0"
+    AddZeroMacro(module, CLONE_NEWTIME);
+#endif /* CLONE_NEWTIME */
+#ifdef CLONE_NEWNS
     PyModule_AddIntMacro(module, CLONE_NEWNS);
+#else
+# warning "CLONE_NEWNS macro not found. Set to 0"
+    AddZeroMacro(module, CLONE_NEWNS);
+#endif /* CLONE_NEWNS */
+#ifdef CLONE_NEWCGROUP
     PyModule_AddIntMacro(module, CLONE_NEWCGROUP);
+#else
+# warning "CLONE_NEWCGROUP macro not found. Set to 0"
+    AddZeroMacro(module, CLONE_NEWCGROUP);
+#endif /* CLONE_NEWCGROUP */
+#ifdef CLONE_NEWUTS
     PyModule_AddIntMacro(module, CLONE_NEWUTS);
+#else
+# warning "CLONE_NEWUTS macro not found. Set to 0"
+    AddZeroMacro(module, CLONE_NEWUTS);
+#endif /* CLONE_NEWUTS */
+#ifdef CLONE_NEWIPC
     PyModule_AddIntMacro(module, CLONE_NEWIPC);
+#else
+# warning "CLONE_NEWIPC macro not found. Set to 0"
+    AddZeroMacro(module, CLONE_NEWIPC);
+#endif /* CLONE_NEWIPC */
+#ifdef CLONE_NEWUSER
     PyModule_AddIntMacro(module, CLONE_NEWUSER);
+#else
+# warning "CLONE_NEWUSER macro not found. Set to 0"
+    AddZeroMacro(module, CLONE_NEWUSER);
+#endif /* CLONE_NEWUSER */
+#ifdef CLONE_NEWPID
     PyModule_AddIntMacro(module, CLONE_NEWPID);
+#else
+# warning "CLONE_NEWPID macro not found. Set to 0"
+    AddZeroMacro(module, CLONE_NEWPID);
+#endif /* CLONE_NEWPID */
+#ifdef CLONE_NEWNET
     PyModule_AddIntMacro(module, CLONE_NEWNET);
+#else
+# warning "CLONE_NEWNET macro not found. Set to 0"
+    AddZeroMacro(module, CLONE_NEWNET);
+#endif /* CLONE_NEWNET */
 
     return module;
 }
